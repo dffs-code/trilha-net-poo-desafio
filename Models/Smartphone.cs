f4814f6a94753b1733f92a9a8d4a6840c209ffa7
Daniel Formigoni <d.soares@aluno.ifsp.edu.br>
2024-08-29 17:49:04 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,20 @@
+using System;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Numero { get; set; }  // Propriedade pública
+        private string Modelo { get; set; } // Propriedade privada
+        private string IMEI { get; set; }   // Propriedade privada
+        public int Memoria { get; set; }    // Propriedade pública, representando a memória em GB
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
